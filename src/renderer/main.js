6a8f24d92f0fb625d3c60510df68c02a995d587d
Krzysztof Goliński <krzysztof@golinski.org>
2018-05-13 15:50:38 +0200

    another approach

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import webFrame from 'electron'
+
+const {webFrame} = require('electron')
 
 webFrame.setVisualZoomLevelLimits(1, 1)
 webFrame.setLayoutZoomLevelLimits(0, 0)
